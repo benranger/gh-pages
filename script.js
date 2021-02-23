9621f9aff418ab903bdbd4e279ddfeaac5bbc677
Ben <benranger111@gmail.com>
2021-02-23 12:56:31 -0500

    Added mobile stylesheet + logic

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    alert("mobile");
-   }
-else {
-    alert("desktop");
+if(/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) mobilePage();
+
+function mobilePage() {
+    $('link[href="desktop-stylesheet.css"]').remove();
+    $('head').append('<link href="mobile-stylesheet.css" rel="stylesheet">');
 }
 
-setInterval(logger, 10);
+//Underscore Animation
+setInterval(underscoreAnim, 10);
 let op = 1
 let factor = -.01;
-function logger() {
+function underscoreAnim() {
     //console.log(document.getElementById("underscore").style.opacity);
     if (op <= 0) factor = .01;
     if (op >= 1) factor = -.01;
@@ -16,6 +17,7 @@ function logger() {
     document.getElementById("underscore").style.opacity = op;
 }
 
+//Arrows Animation
 function hovering(x) {
     if (x>0) {
         document.getElementById("link-arrow-"+x).style.padding = "0 0 0 3px";
